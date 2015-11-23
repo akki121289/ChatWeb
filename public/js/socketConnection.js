cbfc5e2f828113add3cbb53a4e20a87c5772df49
Deepak Rai <deepak.rai@daffodilsw.com>
2015-11-23 17:19:08 +0530

    minimize window

--- public/js/socketConnection.js
+++ public/js/socketConnection.js
@@ -107,7 +107,7 @@ $(document).ready(function(){
 function CreateTab(name,userId)
 {    
       var Id = userId.substr(0,userId.indexOf('@'));
-      $('#chat_tabs').append('<form data-attribute="'+Id+'" id="'+Id+'" class="personalMsgForm"><div class=col-sm-3 style="border:1px solid black;background:white;"><div> <div class=col-sm-12 style="background:green;">  <span class="glyphicon glyphicon-minus" style="float: right;" aria-hidden="true"></span>  <span class="glyphicon glyphicon-unchecked" style="float: right;" aria-hidden="true"></span>  <span class="glyphicon glyphicon-remove" style="float: right;" aria-hidden="true"></span> </div>    <div>'+ name +'</div><div style="width:80%;float:left;"> <ul class="personalMessages" style="padding-bottom:40px"></ul></div><div style="width:20%; margin-left:auto;"><div style="width:100%; margin-left: auto;"></div></div></div><div><input class="personalMessage" autocomplete="off" placeholder="Type message" class="form-control"><button>Send</button></div> </div></form>')
+      $('#chat_tabs').append('<form data-attribute="'+Id+'" id="'+Id+'" class="personalMsgForm"><div class=col-sm-3 style="border:1px solid black;background:white;"><div> <div class=col-sm-12 style="background:green;">  <span class="glyphicon glyphicon-minus" onclick="hideTab(this)" style="float: right;" aria-hidden="true"></span>  <span class="glyphicon glyphicon-unchecked" style="float: right;" aria-hidden="true"></span>  <span class="glyphicon glyphicon-remove" style="float: right;" aria-hidden="true"></span> </div>    <div>'+ name +'</div><div class="hideable" style="width:80%;float:left;"> <ul class="personalMessages" style="padding-bottom:40px"></ul></div></div><div class="hideable" ><input class="personalMessage" autocomplete="off" placeholder="Type message" class="form-control"><button>Send</button></div> </div></form>')
       $('.personalMsgForm').submit(function(e){
             e.preventDefault();
             alert("okkkkkk");
@@ -126,4 +126,19 @@ function tabMessageSender()
                   socket.emit('message',{msg:message});
                   $('#messageTab').val('');
             } 
+}
+
+function hideTab(parr)
+{
+      // console.log("hellllllllllllllllllllooooooooooooooo");
+      // console.log($(this).children());
+      var elements = $(parr).parent().parent().children();
+      elements.each(function(index){
+            console.log($(this));
+            if( $(this).hasClass( "hideable" ))
+            $(this).hide();
+      })
+      
+      // elements('.hideable').hide();
+      // console.log($(parr).parent().parent().children().('.hideable').hide());
 }
