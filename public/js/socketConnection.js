7400c496df5026a787e7b3921e34880dd2b07afd
Deepak Rai <deepak.rai@daffodilsw.com>
2015-11-24 11:29:28 +0530

    only 1 tab open for one person

--- public/js/socketConnection.js
+++ public/js/socketConnection.js
@@ -126,15 +126,15 @@ function CreateTab(name,userId)
       }else{
             Id = userId;
       }
-     // var aa = [];
-     //  $('#chat_tabs').children().each(function(index){
-     //        // console.log($(this));
-     //        aa.push($(this).attr('id'));
-     //        // window.currenTab = $(this).attr('id');
-     //  });      
+     var aa = [];
+      $('#chat_tabs').children().each(function(index){
+            // console.log($(this));
+            aa.push($(this).attr('id'));
+            // window.currenTab = $(this).attr('id');
+      });      
 
-     //  console.log(aa);
-     //  if(jQuery.inArray(Id, aa) == -1)
+      console.log(aa);
+      if(jQuery.inArray(Id, aa) == -1)
       // if(jQuery.inArray(Id, window.currenTab) == -1)
             $('#chat_tabs').append('<form data-attribute="'+Id+'" id="'+Id+'" class="personalMsgForm"><div class=col-sm-3 style="border:1px solid black;background:white;"><div> <div class=col-sm-12 style="background:green;">  <span class="glyphicon glyphicon-minus" onclick="hideTab(this)" style="float: right;" aria-hidden="true"></span>  <span class="glyphicon glyphicon-unchecked" style="float: right;" aria-hidden="true" onclick="showTab(this)" ></span>  <span class="glyphicon glyphicon-remove" style="float: right;" aria-hidden="true" onclick="removeTab(this)"></span> </div>    <div>'+ name +'</div><div class="hideable" style="width:100%;float:left;height:110px;overflow: scroll;"> <ul class="personalMessages" style="padding-bottom:40px"></ul></div></div><div class="hideable" ><input class="personalMessage" autocomplete="off" placeholder="Type message" class="form-control"><button>Send</button></div> </div></form>')
 
